80079ba7019edfca0be9fe64d7f1fab8b748bf92
KIM SIM LAM <chrislam1112@gmail.com>
2025-02-28 17:51:17 +0800

    fixed quote

--- proposal-game/pages/congratulations.js
+++ proposal-game/pages/congratulations.js
@@ -54,10 +54,6 @@ export default function Congratulations() {
 			<Head>
 				<title>Congratulations&#33; 💕</title>
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
-				<link
-					href='https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap'
-					rel='stylesheet'
-				/>
 			</Head>
 
 			<main className='text-center'>
--- proposal-game/pages/index.js
+++ proposal-game/pages/index.js
@@ -327,10 +327,6 @@ export default function Home() {
 			<Head>
 				<title>Be My Girl?</title>
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
-				<link
-					href='https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap'
-					rel='stylesheet'
-				/>
 			</Head>
 
 			<main className='w-full max-w-md mx-auto text-center'>
